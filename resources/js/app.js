ea6986f3171ed0b65c8f046b9ae963b5dc91b317
andrealivornese <andrealivornese@gmail.com>
2024-04-08 17:04:01 +0200

    importazione dell'header e del jumbo

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,3 +7,19 @@ import '~resources/scss/app.scss';
 import.meta.glob([
     '../img/**'
 ]);
+
+import * as bootstrap from 'bootstrap';
+
+
+const { createApp } =  Vue
+
+  createApp({
+    data() {
+        return {
+            navLinks : [
+                "Characters","Comics", "movies", "tv", "Games", "Collectibles", "videos", "Fans", "news", "Shop" 
+            ],
+        }
+    },
+  }).mount('#app');
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -5,10 +8,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title></title>
+    @vite('resources/js/app.js')
 </head>
 <body>
+    <!-- vue script -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
+    <div id="app">
+        <nav>
+            <div class="container">
+                <div class="my_row">
+                    <div>
+                        <img src="{{Vite::asset("resources/img/dc-logo.png")}}" alt="">
+                    </div>
+                    <div>
+                        <ul class="links">
+                            <li v-for="currentLink in navLinks">
+                                <a href="">@{{currentLink}}</a>
+                            </li>
+                        </ul>
+                    </div>
+        
+                </div>
+        
+            </div>
+        </nav>
+
+
+        {{-- jumbo --}}
+
+
+        <section class="sec-jumbotron">
+
+            <div id="jumbotron-img">
+              <img src="{{Vite::asset("resources/img/jumbotron.jpg")}}" alt="jumbotron">
+            </div>
+
+        </section>
+
+
+        
+    </div>
+
+
+
+    
     @yield('content')
+ 
+
+
     
 </body>
 </html>
