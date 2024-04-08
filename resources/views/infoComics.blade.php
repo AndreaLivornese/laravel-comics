42de5373b1146a49b9f312acdc52dbdef5e4d1b4
andrealivornese <andrealivornese@gmail.com>
2024-04-09 01:00:50 +0200

    creazione della seconda tabella e aggiunta del colore secondario

--- resources/views/infoComics.blade.php
+++ resources/views/infoComics.blade.php
@@ -46,28 +46,63 @@
                <table class="my_table">
 
                 <thead>
-                    <tr>
-                        <th colspan="2">
-                            Talent
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Art by:</td>
-                        <td>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, voluptas fugit, illo minus labore dolore </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Written by:
-                        </td>
-                        <td>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis quia repellendus ea minus 
-                        </td>
-                    </tr>
-                </tbody>
+                        <tr>
+                            <th colspan="2">
+                                Talent
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Art by:</td>
+                            <td>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, voluptas fugit, illo minus labore dolore </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Written by:
+                            </td>
+                            <td>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis quia repellendus ea minus 
+                            </td>
+                        </tr>
+                    </tbody>
 
-            </table>
+                </table>
+
+
+                <table class="my_table">
+
+                    <thead>
+                        <tr>
+                            <th colspan="2">
+                                Specs
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Series:</td>
+                            <td>{{$comics[0]["series"]}}</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                U.S. Price:
+                            </td>
+                            <td>
+                                {{$comics[0]["price"]}}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                               On Sale Date:
+                            </td>
+                            <td>
+                                {{$comics[0]["sale_date"]}} 
+                            </td>
+                        </tr>
+                    </tbody>
+
+                </table>
 
             </div>
 
